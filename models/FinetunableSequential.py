47cc0d3eb8cca5af4be1d957cb5eebed31914d19
khalafawyy <moyle2010@gmail.com>
2019-06-19 17:16:30 +0200

    Finetunable Sequential

--- models/FinetunableSequential.py
+++ models/FinetunableSequential.py
@@ -1,9 +1,45 @@
 from keras.models import Sequential
-
+from keras.layers import Dense
 
 class FinetunableSequential(Sequential):
 
 
-    def finetune(X=X_train, y=y_train, validation_data=None, gradual_unreezing=False, Slanted_Triangular_Learning_rate=False):
+    def finetune (self,
+            x=None,
+            y=None,
+            new_class_count=None,
+            batch_size=None,
+            epochs=1,
+            verbose=1,
+            callbacks=None,
+            validation_split=0.,
+            validation_data=None,
+            shuffle=True,
+            class_weight=None,
+            sample_weight=None,
+            initial_epoch=0,
+            steps_per_epoch=None,
+            validation_steps=None,
+            validation_freq=1,
+            max_queue_size=10,
+            workers=1,
+            use_multiprocessing=False,
+            slanted_triangular_learning_rates=False,
+            discriminative_finetuning=False,
+            gradual_unfreezing=False,
 
-        pass
+            **kwargs):
+
+        assert new_class_count is not None
+
+        self.pop()
+        # popping the last layer 
+        self.add(Dense(2, activation='softmax'))
+        self.compile(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
+
+        self.fit(X,y) 
+
+
+if __name__ =='__main__':
+
+    pass
